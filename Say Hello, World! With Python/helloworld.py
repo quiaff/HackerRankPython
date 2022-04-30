474d4ebb116a9343dd809601248c239feef58ebf
Nayiby A. Rojas M <quiaff@gmail.com>
2022-04-30 01:31:22 -0300

    Hello world

---
+++ Say Hello, World! With Python/helloworld.py
@@ -0,0 +1,22 @@
+'''print("Hello, World!")
+You can just as easily store a string as a variable and then print it to stdout:
+
+my_string = "Hello, World!"
+print(my_string)
+The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output 0
+
+Hello, World! 
+'''
+
+name=input("Please enter your name: ")
+print(f"Welcome to Python {name}!")
